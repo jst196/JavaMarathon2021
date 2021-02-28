4eaaa0402bfd6895f97252de7c1ade3331b5adfb
jst196 <jst1749.7@yandex.ru>
2021-02-28 12:55:28 +0500

    j

--- src/main/java/day17/Task1.java
+++ src/main/java/day17/Task1.java
@@ -1,7 +1,18 @@
 package day17;
 
+import java.util.Arrays;
+
 public class Task1 {
     public static void main(String[] args) {
+        String[] chees = {CheesPiece.PAWN_WHITE.getName(),
+                CheesPiece.PAWN_WHITE.getName(),
+                CheesPiece.PAWN_WHITE.getName(),
+                CheesPiece.PAWN_WHITE.getName(),
+                CheesPiece.ROOK_BLACK.getName(),
+                CheesPiece.ROOK_BLACK.getName(),
+                CheesPiece.ROOK_BLACK.getName(),
+                CheesPiece.ROOK_BLACK.getName()};
 
+        System.out.println(Arrays.toString(chees));
     }
 }
--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -3,5 +3,15 @@ package day8;
 public class Task1 {
     public static void main(String[] args) {
 
+        String string = "";
+
+
+        for (int i = 0; i <= 20000; i++) {
+
+            string += i + " ";
+
+        }
+        System.out.print(string);
+
     }
 }
